dd7269103246f7f5306353e826d01c7305510d08
Your Name <you@example.com>
2020-12-18 16:06:52 +0100

    update pokemon

--- src/include/pokemon.hpp
+++ src/include/pokemon.hpp
@@ -21,20 +21,20 @@ private:
 
 public:
   /// ASD
-  double      getHealth_() { return this.health_; }
-  void        setHealth_(double health_) { this.health_ = health_; }
-  int         getLevel_() { return this.level_; }
-  void        setLevel_(int level_) { this.level_ = level_; }
-  int         getXp_() { return this.xp_; }
-  void        setXp_(int xp_) { this.xp_ = xp_; }
-  double      getAttack_() { return this.attack_; }
-  void        setAttack_(double attack_) { this.attack_ = attack_; }
-  double      getDefense_() { return this.defense_; }
-  void        setDefense_(double defense_) { this.defense_ = defense_; }
-  std::string getName_() { return this.name_; }
-  void        setName_(std::string name_) { this.name_ = name_; }
-  std::string getNickname_() { return this.nickname_; }
-  void setNickname_(std::string nickname_) { this.nickname_ = nickname_; }
+  const double      getHealth_() const { return this->health_; }
+  void        setHealth_(double health_) { this->health_ = health_; }
+  const int         getLevel_() const { return this->level_; }
+  void        setLevel_(int level_) { this->level_ = level_; }
+  const int         getXp_() const { return this->xp_; }
+  void        setXp_(int xp_) { this->xp_ = xp_; }
+  const double      getAttack_() const{ return this->attack_; }
+  void        setAttack_(double attack_) { this->attack_ = attack_; }
+  const double      getDefense_() const { return this->defense_; }
+  void        setDefense_(double defense_) { this->defense_ = defense_; }
+  const std::string getName_() const { return this->name_; }
+  void        setName_(std::string name_) { this->name_ = name_; }
+  const std::string getNickname_() const { return this->nickname_; }
+  void setNickname_(std::string nickname_) { this->nickname_ = nickname_; }
 
   PrimaryType   primaryType_;
   SecondaryType secondaryType_;
@@ -49,10 +49,16 @@ public:
     static_assert(Contains<poketypes::PokemonTypeList, SecondaryType>::value, "Must be a PokemonType");
     this->health_ = health;
     this->level_  = level;
+    this->name_ = name;
+    this->nickname_ = nickname;
+    this->xp_ = xp;
+    this->attack_ = attack;
+    this->defense_ = defense;
   }
 
   Pokemon() {}
   ~Pokemon() {}
+
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "include/pokemon.hpp"
 #include "include/poke_type.hpp"
+#include <iostream>
 
 int main() {
 // double health, unsigned int level, unsigned int xp, double attack, double defense, std::string name, std::string nickname){
         
-    Pokemon<int, poketypes::WaterPokemonType> p(100.2,10,0,50,100,"Jonathan", "Squirtle");  
-
+    Pokemon<poketypes::GroundPokemonType, poketypes::WaterPokemonType> p(100.2,10,0,50,100,"Jonathan", "Squirtle");  
+    std::cout << "Pokemon health: " << p.getHealth_() << std::endl;
+    
     return 0;
 }
