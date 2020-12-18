13f2edb098a3585085b0440dd8facaea0a9fae1c
Your Name <you@example.com>
2020-12-18 13:32:02 +0100

    pokemon

--- src/include/pokemon.hpp
+++ src/include/pokemon.hpp
@@ -1,16 +1,45 @@
+#include <string>
+#include <iostream>
+// #include "poketype.hpp"
+template<typename PrimaryType, typename SecondaryType>
 class pokemon
 {
 private:
     /* data */
+  double       health_;
+  unsigned int level_;
+  unsigned int xp_;
+  double       attack_;
+  double       defense_;
+  std::string  name_;
+  std::string  nickname_;
+
 public:
-    pokemon(/* args */);
+    /// ASD
+    double getHealth_() { return this.health_; }
+    void setHealth_(double health_) { this.health_ = health_; }
+    int getLevel_() { return this.level_; }
+    void setLevel_(int level_) { this.level_ = level_; }
+    int getXp_() { return this.xp_; }
+    void setXp_(int xp_) { this.xp_ = xp_; }
+    double getAttack_() { return this.attack_; }
+    void setAttack_(double attack_) { this.attack_ = attack_; }
+    double getDefense_() { return this.defense_; }
+    void setDefense_(double defense_) { this.defense_ = defense_; }
+    std::string getName_() { return this.name_; }
+    void setName_(std::string name_) { this.name_ = name_; }
+    std::string getNickname_() { return this.nickname_; }
+    void setNickname_(std::string nickname_) { this.nickname_ = nickname_; }
+
+    PrimaryType   primaryType_;
+    SecondaryType secondaryType_;
+    pokemon(double health, unsigned int level, unsigned int xp, double attack, double defense, std::string name, std::string nickname){
+        static_assert(std::is_same_v<PrimaryType, PokeType>);
+        static_assert(std::is_same_v<SecondaryType, PokeType>);
+        this.health_ = health;
+        this->level_ = level;
+    }
+
+    pokemon();
     ~pokemon();
 };
-
-pokemon::pokemon(/* args */)
-{
-}
-
-pokemon::~pokemon()
-{
-}
