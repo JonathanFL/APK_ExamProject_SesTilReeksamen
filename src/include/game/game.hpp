3d3789db8c2dcdb1214e843c4094891ce0557ce1
valdemartang <valdemar-tang@hotmail.com>
2020-12-28 11:30:50 +0100

    Added while loop to PokeCenter

--- src/include/game/game.hpp
+++ src/include/game/game.hpp
@@ -22,121 +22,124 @@
 class PokemonGame
 {
 private:
-    std::map<std::string, PokemonList> pokemons;
-    dbloader::PokemonLoader pokemonLoader;
-    PokeBag bag_;
-    std::shared_ptr<PokeCenter::Center> center_;
-    Player player_;
+  std::map<std::string, PokemonList>  pokemons;
+  dbloader::PokemonLoader             pokemonLoader;
+  PokeBag                             bag_;
+  std::shared_ptr<PokeCenter::Center> center_;
+  Player                              player_;
 
 public:
-    PokemonGame(){};
-    ~PokemonGame(){};
+  PokemonGame(){};
+  ~PokemonGame(){};
 
-    void loadPokemons()
+  void loadPokemons()
+  {
+    std::cout << "=================================" << std::endl;
+    std::cout << "=  WELCOME TO THE POKEMON GAME  =" << std::endl;
+    std::cout << "=           SIMULATOR           =" << std::endl;
+    std::cout << "=================================" << std::endl;
+
+    std::cout << std::endl << "Loading game setup..." << std::endl << std::endl;
+
+    try
     {
-        std::cout << "=================================" << std::endl;
-        std::cout << "=  WELCOME TO THE POKEMON GAME  =" << std::endl;
-        std::cout << "=           SIMULATOR           =" << std::endl;
-        std::cout << "=================================" << std::endl;
-
-        std::cout << std::endl
-                  << "Loading game setup..." << std::endl
-                  << std::endl;
-
-        try
-        {
-            pokemonLoader.ReadPokemonsList(PokemonListEntry::LowLevelPokemons, PokemonListEntry::MediumLevelPokemons, PokemonListEntry::HighLevelPokemons, PokemonListEntry::RarePokemons, PokemonListEntry::UserPokemons);
-            pokemons = pokemonLoader.getPokemons();
-            bag_.addPokemons(std::forward<PokemonList>(pokemons[PokemonListEntry::UserPokemons]));
-        }
-        catch (poketypes::UnknownPokemonTypeException &e)
-        {
-            std::cout << "UnknownPokemonTypeException: " << e.what() << std::endl;
-            throw;
-        }
-        catch (dbloader::FileNotFoundException &e)
-        {
-            std::cout << "FileNotFoundException: " << e.what() << std::endl;
-            throw;
-        }
-        catch (...)
-        {
-            std::cout << "Unknown error on reading pokemons" << std::endl;
-            throw;
-        }
+      pokemonLoader.ReadPokemonsList(PokemonListEntry::LowLevelPokemons,
+                                     PokemonListEntry::MediumLevelPokemons,
+                                     PokemonListEntry::HighLevelPokemons,
+                                     PokemonListEntry::RarePokemons,
+                                     PokemonListEntry::UserPokemons);
+      pokemons = pokemonLoader.getPokemons();
+      bag_.addPokemons(
+          std::forward<PokemonList>(pokemons[PokemonListEntry::UserPokemons]));
     }
-
-    void setPokeCenter(std::shared_ptr<PokeCenter::Center> &&center)
+    catch (poketypes::UnknownPokemonTypeException &e)
     {
-        center_ = std::forward<std::shared_ptr<PokeCenter::Center>>(center);
+      std::cout << "UnknownPokemonTypeException: " << e.what() << std::endl;
+      throw;
     }
-
-    void initPlayer()
+    catch (dbloader::FileNotFoundException &e)
     {
-        std::string name;
-        std::cout << "Enter your player name: ";
-        std::cin >> name;
-
-        std::cout << "Welcome to the game " << name << std::endl
-                  << std::endl;
-
-        player_.setName(std::forward<std::string>(name));
-        player_.setBag(bag_);
+      std::cout << "FileNotFoundException: " << e.what() << std::endl;
+      throw;
     }
-
-    void run()
+    catch (...)
     {
-        bool exit = false;
-        while (!exit) // Game loop
+      std::cout << "Unknown error on reading pokemons" << std::endl;
+      throw;
+    }
+  }
+
+  void setPokeCenter(std::shared_ptr<PokeCenter::Center> &&center)
+  {
+    center_ = std::forward<std::shared_ptr<PokeCenter::Center>>(center);
+  }
+
+  void initPlayer()
+  {
+    std::string name;
+    std::cout << "Enter your player name: ";
+    std::cin >> name;
+
+    std::cout << "Welcome to the game " << name << std::endl << std::endl;
+
+    player_.setName(std::forward<std::string>(name));
+    player_.setBag(bag_);
+  }
+
+  void run()
+  {
+    bool exit = false;
+    while (!exit) // Game loop
+    {
+      std::future<PlayerGameChoice> choiceFuture = player_.makeGameChoice();
+      choiceFuture.wait();
+      PlayerGameChoice choice = choiceFuture.get();
+      switch (choice)
+      {
+      case PlayerGameChoice::GoToPokecenter:
+      {
+        center_->usePokecenter(player_);
+        break;
+      }
+      case PlayerGameChoice::BattleWildPokemon:
+      {
+        PokemonList::iterator randomPokemon = Utilities::select_random_if(
+            pokemons[PokemonListEntry::RarePokemons].begin(),
+            pokemons[PokemonListEntry::RarePokemons].end(),
+            [](const Pokemon &p) { return p.getHealth_() > 0; });
+        if (randomPokemon == pokemons[PokemonListEntry::RarePokemons].end())
         {
-            std::future<PlayerGameChoice> choiceFuture = player_.makeGameChoice();
-            choiceFuture.wait();
-            PlayerGameChoice choice = choiceFuture.get();
-            switch (choice)
-            {
-            case PlayerGameChoice::GoToPokecenter:
-            {
-                center_->usePokecenter(player_);
-                break;
-            }
-            case PlayerGameChoice::BattleWildPokemon:
-            {
-                PokemonList::iterator randomPokemon =
-                    Utilities::select_random_if(pokemons[PokemonListEntry::RarePokemons].begin(), pokemons[PokemonListEntry::RarePokemons].end(),
-                                                [](const Pokemon &p) { return p.getHealth_() > 0; });
-                if (randomPokemon == pokemons[PokemonListEntry::RarePokemons].end())
-                {
-                    std::cout << "No wild pokemon left - you have won the game!"
-                              << std::endl;
-                    exit = true;
-                }
-                else if (!player_.canBattle())
-                {
-                    std::cout << "You either have no pokemon, or they have all fainted."
-                              << std::endl;
-                }
-                else
-                {
-                    battle::playBattle(&player_, &*randomPokemon);
-                }
-                break;
-            }
-            case PlayerGameChoice::CheckPokemons:
-            {
-                player_.listPokemon();
-                break;
-            }
-            case PlayerGameChoice::CheckBagItems:
-            {
-                player_.listBagItems();
-                break;
-            }
-            case PlayerGameChoice::ExitGame:
-            {
-                exit = true;
-                break;
-            }
-            }
+          std::cout << "No wild pokemon left - you have won the game!"
+                    << std::endl;
+          exit = true;
         }
+        else if (!player_.canBattle())
+        {
+          std::cout << "You either have no pokemon, or they have all fainted."
+                    << std::endl;
+        }
+        else
+        {
+          battle::playBattle(&player_, &*randomPokemon);
+        }
+        break;
+      }
+      case PlayerGameChoice::CheckPokemons:
+      {
+        player_.listPokemon();
+        break;
+      }
+      case PlayerGameChoice::CheckBagItems:
+      {
+        player_.listBagItems();
+        break;
+      }
+      case PlayerGameChoice::ExitGame:
+      {
+        exit = true;
+        break;
+      }
+      }
     }
+  }
 };
